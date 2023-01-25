0d0a3b5b23c8ccc1dc8b83a4ee1d1904b5b184a6
PRANAV <pranavprasanth907@gmail.com>
2023-01-26 01:36:55 +0530

    made messages dynamic,set time format using moment, implement scroll and focusing input field after sending message

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const  path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+const botName = 'Mchat Bot';
 
 io.on('connection', socket => {
     //welcome current user
-    socket.emit('message', 'Welcome to M chat');
+    socket.emit('message', formatMessage(botName, 'Welcome to M chat'));
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat.');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat.'));
     //Runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat.');
+        io.emit('message', formatMessage(botName,  'A user has left the chat.'));
+    })
+    // listen for chat message
+    socket.on('chatMessage', msg => {
+        io.emit('message' , formatMessage('USER', msg));    
     })
 
 
